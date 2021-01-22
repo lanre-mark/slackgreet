b693791fb3acbda3861e71c87eec7c46ed9b2909
olanrewaju Makinde <lanre.developer@gmail.com>
2021-01-22 21:29:28 +0100

    initialized server lib project and libs

---
+++ server/settings.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  dbString: 'postgres://postgres:postgres@localhost:5432',
+  domain: 'http://localhost:8791', //8791, 8792, 8793, 8794, 8795, 8796, 8798, 8799
+  logFile: './slack-adapter.log',
+  SLACK_APP_ID: 'A01KG7MP78D',
+  SLACK_CLIENT_ID: '930801607271.1662259789285',
+  SLACK_CLIENT_SECRET: 'a5d9ea1f4e613274aaae1355eb2774a1',
+  SLACK_SIGNING_SECRET: '7c43db6b763917c27be2706f5d70349d',
+  SLACK_VERIFICATION_TOKEN: 'I8S1f7OsYxXxhkiwdPK6FXqg',
+}
